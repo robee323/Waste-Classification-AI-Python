91e366a5330d6255277c49f2563d42f8fcd09357
robee323 <robee@op.pl>
2022-06-14 02:21:11 +0200

    Improving the accuracy of the CNN model, adding Confusion Matrix, Training Loss Graph, Model's Predicition Testing, ModelCheckpoint and Accuracy score
    The next stage of work on the project was to test the prediction of the created model, the implementation of the model prediction testing was performed. The prediction results are presented in a graphical form (table with images and their labels and prediction results). Due to the non-satifying prediction results, work was continued on increasing the actual accuracy of the model. For this purpose, the CNN model was reduced by one layer and the filters in the remaining layers were changed (respectively 32,64,32). The dense layer was changed from 128 to 64. The configuration parameters of the input data augmentation within ImageDataGenerator were changed.  Dropout class and validation_steps parameter in fit class have been dropped. The size of the image entering the shell has been increased from (100,100) to (300, 300), batch_size has been changed from 40 to 32, the number of epochs has been increased from 20 to 100. The ratio of train and test subsets has also been reduced. In the most important categories, the ratio is now 90% train, 10% test. These decisions were motivated by the fact that the dataset consists of a relatively small number of elements and therefore the learning time should be extended and the detail of data entering the model should be increased (by reducing their compression). The padding='same' attribute has been added to the Conv2D class so that no columns or rows of source images are skipped during analysis. In addition, the ModelCheckpoint class has been added so that only results from epochs where better performance was achieved than before can be saved. These steps have resulted in a decrease in training loss and an increase in the actual accuracy of the model during testing - this is calculated using the accuracy score (the prediction label created by the trained model is compared to the label assigned to the image within the ImageDataGenerator).  The accuracy of the model itself according to the logs from the fit class remained at a similar level to the previous model. In order to better understand how the model classifies test images, a confusion matrix has been implemented, the purpose of which is to graphically represent the model's prediction and understand which categories of images it has the most trouble with. To the existing accuracy graph drawing, a training loss graph drawing has also been added.

--- main.py
+++ main.py
@@ -41,86 +41,130 @@ for i, img_path in enumerate(next_rock+next_paper+next_scissors):
 ###################################################################################################
 
 import tensorflow as tf
-import keras_preprocessing
+import numpy as np
+import cv2
+import keras
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras_preprocessing import image
 from keras_preprocessing.image import ImageDataGenerator
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"  
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 TRAINING_DIR = "./dataset/train/"
 training_datagen = ImageDataGenerator(
       rescale = 1./255,
-	  rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
+	    #rotation_range=40,
+      validation_split=0.1,
+      width_shift_range=0.1,
+      height_shift_range=0.1,
+      shear_range=0.1,
+      zoom_range=0.1,
       horizontal_flip=True,
-      fill_mode='nearest')
+      vertical_flip=True)
 
 VALIDATION_DIR = "./dataset/test/"
-validation_datagen = ImageDataGenerator(rescale = 1./255)
+validation_datagen = ImageDataGenerator(rescale = 1./255, validation_split=0.1)
 
 train_generator = training_datagen.flow_from_directory(
 	TRAINING_DIR,
-	target_size=(100,100),
+	target_size=(300,300),
 	class_mode='categorical',
-  batch_size=40
+  batch_size=32
   )
 
 validation_generator = validation_datagen.flow_from_directory(
 	VALIDATION_DIR,
-	target_size=(100,100),
+	target_size=(300,300),
 	class_mode='categorical',
-  batch_size=40
+  batch_size=32
 )
+labels = (train_generator.class_indices)
+labels = dict((v,k)for k,v in labels.items())
 
 model = tf.keras.models.Sequential([
-    # Note the input shape is the desired size of the image 100x100 with 3 bytes color
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
     # The first convolution
-    tf.keras.layers.Conv2D(128, (1,1), activation='relu', input_shape=(100, 100, 3)),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(300, 300, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
     # The second convolution
-    tf.keras.layers.Conv2D(256,(1,1), activation='relu'),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
     # The third convolution
-    tf.keras.layers.Conv2D(512, (1,1), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    # The fourth convolution
-    tf.keras.layers.Conv2D(1024, (1,1), activation='relu'),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
     # Flatten the results to feed into a DNN
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dropout(0.5),
-    # 128 neuron hidden layer
-    tf.keras.layers.Dense(128, activation='relu'),
+    # 64 neuron hidden layer
+    tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(6, activation='softmax')
-])
-
+    ])
+filepath="waste_model.h5"
+checkpoint1=ModelCheckpoint(filepath,monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list=[checkpoint1]
 
 model.summary()
 
-model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics=[tf.keras.metrics.categorical_accuracy])
 
-history = model.fit(train_generator, epochs=20, validation_data = validation_generator, verbose = 1, validation_steps=3)
+history = model.fit(train_generator, epochs=100, validation_data = validation_generator, verbose = 1, callbacks=callbacks_list)
 
-model.save("waste.h5")
+file="waste_model.h5"
+keras.models.save_model(model,file)
+
 ####################################################################
+
 import matplotlib.pyplot as plt
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
 
-epochs = range(len(acc))
-
-plt.plot(epochs, acc, 'r', label='Training accuracy')
-plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
-plt.title('Training and validation accuracy')
-plt.legend(loc=0)
-plt.figure()
-
-
+# Training loss graph drawing
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.legend()
 plt.show()
 
-#################################################################
+#Accuracy graph drawing
+plt.plot(history.history['categorical_accuracy'], label='acc')
+plt.plot(history.history['val_categorical_accuracy'], label='val_acc')
+plt.legend()
+plt.show()
+
+# Evaluating the model on test data
+
+filenames = validation_generator.filenames
+nb_samples = len(filenames)
+
+model.evaluate_generator(validation_generator, nb_samples)
+
+# Generating predictions on test data
+
+test_x, test_y = validation_generator.__getitem__(1)
+preds = model.predict(test_x)
+
+# Comparing predcitons with original labels
+
+labels = (train_generator.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+plt.figure(figsize=(16, 16))
+for i in range(16):
+    plt.subplot(4, 4, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.title('pred:%s / truth:%s' % (labels[np.argmax(preds[i])], labels[np.argmax(test_y[i])]))
+    plt.imshow(test_x[i])
+
+# Confusion Matrix
+
+y_pred = model.predict(validation_generator)
+y_pred = np.argmax(y_pred, axis=1)
+print('Confusion Matrix')
+cm = confusion_matrix(validation_generator.classes, y_pred)
+labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+disp.plot(cmap=plt.cm.Blues)
+plt.show()
+
+# Accuracy score
+
+acc = accuracy_score(validation_generator.classes, y_pred)
+print("Accuracy is {} percent".format(round(acc*100,2)))
